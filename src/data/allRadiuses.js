9a099b665e3a26317d2b2c956064648474e539fa
Julien <julien.sebag@me.com>
2022-05-28 17:56:45 +0200

    Fix radiuses in CSS

--- src/data/allRadiuses.js
+++ src/data/allRadiuses.js
@@ -4,7 +4,7 @@ const allRadiuses = [
     { name: "L", css: "var(--radius-l)", value: "12px" },
     { name: "M", css: "var(--radius-m)", value: "8px" },
     { name: "S", css: "var(--radius-s)", value: "4px" },
-    { name: "XS", css: "var(--radius-xs)", value: "8px" },
+    { name: "XS", css: "var(--radius-xs)", value: "2px" },
     { name: "Round", css: "var(--radius-round)", value: "99em" },
 ]
 
